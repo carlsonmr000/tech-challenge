912363e455196207c1374df4ce72c1b3c181eb08
carlsonmr <carlsonmr000@gmail.com>
2021-09-13 15:14:35 -0500

    created react app

--- temperature-converter/client/src/App.js
+++ temperature-converter/client/src/App.js
@@ -1,9 +1,25 @@
+// import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+
+  // const [temp, setTemp] = useState(0);
+  // const [input, setInput] = useState("");
+
+  const handleTemp = () => {
+    
+  }
+
   return (
     <div className="App">
-      { /* get hacking! */ }
+      <form>
+        <input className="c-input" type="text" value />
+        <label>Celsius = </label>
+
+        <input className="f-input" type="text" value />
+        <label>Fahrenheit</label>
+      </form>
     </div>
   );
 }
