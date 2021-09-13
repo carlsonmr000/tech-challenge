5d6b8459641049b4126624880cc2ba8d60cc76fb
carlsonmr <carlsonmr000@gmail.com>
2021-09-13 14:19:59 -0500

    formatted app.js code

--- counter/client/src/App.js
+++ counter/client/src/App.js
@@ -1,19 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-
-
   return (
     <div className="App">
-
       <div className="counter-wrapper">
         <div className="count">{count}</div>
-        <button className="count-button" onClick={() => setCount(count + 1)}>Count</button>
+        <button className="count-button" onClick={() => setCount(count + 1)}>
+          Count
+        </button>
       </div>
-
     </div>
   );
 }
