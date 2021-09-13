383725bf4e7c9eba52da70559d685c3c082acb2d
carlsonmr <carlsonmr000@gmail.com>
2021-09-13 14:15:37 -0500

    created count added styling

--- counter/client/src/App.js
+++ counter/client/src/App.js
@@ -1,14 +1,19 @@
-import "./App.css";
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
 
-  const [result, setResult] = useState(10);
-  const [count, setCount]
+
+
   return (
-
     <div className="App">
-      { /* get hacking! */ }
+
+      <div className="counter-wrapper">
+        <div className="count">{count}</div>
+        <button className="count-button" onClick={() => setCount(count + 1)}>Count</button>
+      </div>
+
     </div>
   );
 }
