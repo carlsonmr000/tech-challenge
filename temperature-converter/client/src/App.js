ade66e73c60de6b265f457a20ae0b6a25eac02ac
carlsonmr <carlsonmr000@gmail.com>
2021-09-13 12:56:37 -0500

    created react app for temperature-converter

--- temperature-converter/client/src/App.js
+++ temperature-converter/client/src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import { useEffect, useState } from "react";
 
 function App() {
-
-  const [result, setResult] = useState(10);
-  const [count, setCount]
   return (
-
     <div className="App">
       { /* get hacking! */ }
     </div>
