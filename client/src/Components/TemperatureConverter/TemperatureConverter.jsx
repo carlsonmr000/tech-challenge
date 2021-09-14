ae616fd2566ba906c0b5b17dbae79a5e9590f1c8
carlsonmr <carlsonmr000@gmail.com>
2021-09-14 03:57:47 -0500

    added conditional rendering if fahrenheit is empty

--- client/src/Components/TemperatureConverter/TemperatureConverter.jsx
+++ client/src/Components/TemperatureConverter/TemperatureConverter.jsx
@@ -5,30 +5,45 @@ function TemperatureConverter() {
   const [celsius, setCelsius] = useState(0);
   const [fahrenheit, setFahrenheit] = useState(0);
   const [beingChanged, setBeingChange] = useState("fahrenheit");
+  const [isNumber, setIsNumber] = useState("false");
+  const [isEmpty, setIsEmpty] = useState("false");
 
   useEffect(() => {
-    console.log(beingChanged);
     updateTemp();
   }, [celsius, fahrenheit]);
 
   const updateTemp = (e) => {
-    const validNumber = !isNaN(+celsius);
-    console.log("is number valid?", validNumber);
-
-    // if(celsius.length === 0) {
-    //   setDisable(!disable)
-    // }
-
+    setIsNumber(true);
     if (beingChanged === "fahrenheit") {
-      console.log("this is fahrenheit", fahrenheit);
       setCelsius(parseInt((fahrenheit - 32) * (5 / 9)), 2);
     } else {
       setFahrenheit(parseInt((9 / 5) * celsius + 32), 2);
-
     }
 
-    // if (beingChanged === "celsius") {
-    //   console.log("this is celsius", celsius);
+    if (fahrenheit.length === 0) {
+      setCelsius("-16");
+      setIsNumber(true);
+      setIsEmpty("empty")
+    } 
+    
+    // else if (fahrenheit.length > 1) {
+    //   setCelsius("true");
+    //   setIsEmpty(true)
+
+    // }
+
+    // if (celsius.length === 0) {
+    //   setFahrenheit("false");
+    //   setIsNumber(false);
+    // } 
+
+    // if (fahrenheit === NaN) {
+    //   setIsNumber(false);
+    // }
+
+    // if (celsius === NaN) {
+    //   setIsNumber(false);
+    //   setIsColor(false)
     // }
   };
 
@@ -38,30 +53,30 @@ function TemperatureConverter() {
       <form>
         <div>
           <input
-            className="c-input"
+            className={isNumber ? "true" : "false"}
+            // className={isColor ? "yes" : "no"}
+            id={isEmpty ? "empty" : "not-empty"}
             type="text"
             value={celsius}
             onChange={(e) => {
               setBeingChange("celsius");
               setCelsius(e.target.value);
-              // setDisable(!disable)
+              setIsNumber(!isNumber);
             }}
           />
-          {/* <button onClick={resetInput}>reset celsius</button> */}
           <label>Celsius=</label>
 
           <input
-            className="f-input"
+            className={isNumber ? "true" : "false"}
+            // id={isEmpty ? "empty" : "not-empty"}
             type="text"
             value={fahrenheit}
-            // disabled={disable}
             onChange={(e) => {
               setBeingChange("fahrenheit");
+              setIsNumber(!isNumber);
               setFahrenheit(e.target.value);
             }}
           />
-          {/* <button onClick={resetInput}>reset fahrenheit</button> */}
-
           <label>Fahrenheit</label>
         </div>
       </form>
