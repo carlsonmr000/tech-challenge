ead470ed0acaf6e55403e142ea12adadd2dcb672
carlsonmr <carlsonmr000@gmail.com>
2021-09-13 20:11:01 -0500

    changed h1 font

--- client/src/Components/TemperatureConverter/TemperatureConverter.jsx
+++ client/src/Components/TemperatureConverter/TemperatureConverter.jsx
@@ -1,52 +1,80 @@
 import { useEffect, useState } from "react";
+import "./TemperatureConverter.css"
 
 function TemperatureConverter() {
-
-
   const [celsius, setCelsius] = useState(0);
   const [fahrenheit, setFahrenheit] = useState(0);
-  const [beingChanged, setBeingChange] = useState('fahrenheit')
-  
-  useEffect(() => {
-    console.log('USEE EFFECT HAPPENING!', beingChanged)
-    updateTemp()
+  const [beingChanged, setBeingChange] = useState("fahrenheit");
+  const [disable, setDisable] = useState(false);
 
-  }, [celsius, fahrenheit])
+
+  useEffect(() => {
+    console.log(beingChanged);
+    updateTemp();
+  }, [celsius, fahrenheit]);
+
 
   const updateTemp = (e) => {
-   
-    //e.preventDefault();
-    const formValid = !isNaN(+celsius);
-    if(!formValid) {
-      return;
+    const validNumber = !isNaN(celsius);
+    if (!validNumber) {
+         ;
     }
-    console.log('Fahrenheit!', fahrenheit)
-    if(beingChanged === "fahrenheit") {
-      setCelsius(parseInt((fahrenheit - 32) * ( 5 / 9)), 2)
-    } else {
-      setFahrenheit(parseInt(celsius * ( 9 / 5) + 32), 2)
-    }
-  }
-  
+
  
+    // if(fahrenheit.length === 0) {
+    //   setDisable(!disable)
+    // } else {
+    //   setDisable(disable)
+    // }
+
+    // if(celsius.length === 0) {
+    //   setDisable(!disable)
+    // }
+
+    if (beingChanged === "fahrenheit") {
+      setCelsius(parseInt((fahrenheit - 32) * (5 / 9)), 2);
+    } else {
+      setFahrenheit(parseInt(celsius * (9 / 5) + 32), 2);
+    }
+
+    if(fahrenheit.length === 0) {
+      setDisable(!disable)
+
+    }
+  };
 
   return (
-    <div className="App">
+    <div className="temp-wrapper">
       <h1>Temperature Converter Auto</h1>
       <form>
         <div>
-        <input className="c-input" type="text" value={celsius} onChange={(e) => {
-          setBeingChange('celsius')
-          setCelsius(e.target.value)}
-          } />
-        <label>Celsius = </label>
+          <input
+            className="c-input"
+            type="text"
+            disabled={disable}
+            value={celsius}
+            onChange={(e) => {
+              setBeingChange("celsius");
+              setCelsius(e.target.value);
+            }}
+          />
+          <label>Celsius = </label>
 
-        <input className="f-input" type="text" value={fahrenheit} onChange={(e) => {
-          console.log('About to set Fahrehin state!!', e.target.value)
-          setBeingChange('fahrenheit')
-          setFahrenheit(e.target.value)
-          }} />
-        <label>Fahrenheit</label>
+          <input
+            className="f-input"
+            type="text"
+            disabled={disable}
+            value={fahrenheit}
+            onChange={(e) => {
+              console.log("about to set fahrenheit state!!", e.target.value);
+              setBeingChange("fahrenheit");
+              setFahrenheit(e.target.value);
+            }}
+          />
+          {/* <button type="button" onClick={handleClick}>
+            toggle
+          </button> */}
+          <label>Fahrenheit</label>
         </div>
       </form>
     </div>
