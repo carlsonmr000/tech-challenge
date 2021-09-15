60e1316d5fa5f233879aa14aae1a0f3f00218479
carlsonmr <carlsonmr000@gmail.com>
2021-09-15 16:58:05 -0500

    cleared form on refresh

--- client/src/Components/TemperatureConverter/TemperatureConverter.jsx
+++ client/src/Components/TemperatureConverter/TemperatureConverter.jsx
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react";
 import "./TemperatureConverter.css";
 
 function TemperatureConverter() {
-  const handleClick = (e) => {
-    e.target.select();
-  };
-
-  const [celsius, setCelsius] = useState(" ");
-  const [fahrenheit, setFahrenheit] = useState(" ");
+  const [celsius, setCelsius] = useState();
+  const [fahrenheit, setFahrenheit] = useState();
   const [beingChanged, setBeingChange] = useState("fahrenheit");
-  
+
+  // const handleClick = (e) => {
+  //   e.target.select();
+  // };
+
   useEffect(() => {
     const updateTemp = (e) => {
-
       if (beingChanged === "fahrenheit") {
         setCelsius(parseInt((fahrenheit - 32) * (5 / 9)), 2);
       } else {
@@ -26,60 +25,31 @@ function TemperatureConverter() {
     <div className="temp-wrapper">
       <h1>Temperature Converter Auto</h1>
       <form>
-        {fahrenheit && celsius ? (
-          <div>
-            <input
-              className="number"
-              type="number"
-              value={celsius}
-              onInput={(e) => {
-                setBeingChange("celsius");
-                setCelsius(e.target.value);
-              }}
-              onClick={handleClick}
-            />
-            <label>Celsius=</label>
+        <div>
+          <input
+            className="number"
+            type="number"
+            value={celsius}
+            onInput={(e) => {
+              setBeingChange("celsius");
+              setCelsius(e.target.value);
+            }}
+            // onClick={handleClick}
+          />
+          <label>Celsius=</label>
 
-            <input
-              className="number"
-              type="number"
-              value={fahrenheit}
-              onInput={(e) => {
-                setBeingChange("fahrenheit");
-                setFahrenheit(e.target.value);
-              }}
-              onClick={handleClick}
-            />
-            <label>Fahrenheit</label>
-          </div>
-        ) : (
-          <div>
-            <input
-              className="not-number"
-              type="number"
-              value={celsius}
-              onInput={(e) => {
-                setBeingChange(" ");
-                setCelsius(null);
-              }}
-              onClick={handleClick}
-            />
-            <label>Celsius=</label>
-
-            <input
-              className="not-number"
-              type="number"
-              value={fahrenheit}
-              onInput={(e) => {
-                setBeingChange(" ");
-                setFahrenheit(null);
-              }}
-              onClick={handleClick}
-
-            />
-            <label>Fahrenheit</label>
-          </div>
-        )}
+          <input
+            className="number"
+            type="number"
+            value={fahrenheit}
+            onInput={(e) => {
+              setBeingChange("fahrenheit");
+              setFahrenheit(e.target.value);
+            }}
+            // onClick={handleClick}
+          />
+          <label>Fahrenheit</label>
+        </div>
       </form>
     </div>
   );
